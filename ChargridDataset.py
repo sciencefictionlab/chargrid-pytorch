38914ad70a9a168be03183855914c4179abafc60
Schartz Rehan <rehan.qt@gmail.com>
2020-09-05 16:57:01 +0530

    initial commit

---
+++ ChargridDataset.py
@@ -0,0 +1,132 @@
+import numpy as np
+import torchvision
+from skimage.transform import resize
+from torch.utils.data import Dataset, DataLoader, random_split
+import os
+from PIL import Image
+import torch
+from datetime import datetime
+
+width = 128
+height = 256
+nb_classes = 5
+nb_anchors = 4  # one per foreground class
+input_channels = 61
+base_channels = 64
+batch_size = 5
+
+pad_left_range = 0.2
+pad_top_range = 0.2
+pad_right_range = 0.2
+pad_bot_range = 0.2
+dir_np_chargrid_1h = '/media/schartz/minion/Chargrid/data/np_chargrids_1h'
+dir_np_gt_1h = '/media/schartz/minion/Chargrid/data/np_gt_1h'
+dir_np_bbox_anchor_mask = '/media/schartz/minion/Chargrid/data/np_bbox_anchor_mask'
+dir_np_bbox_anchor_coord = '/media/schartz/minion/Chargrid/data/np_bbox_anchor_coord'
+list_filenames = [f for f in os.listdir(dir_np_chargrid_1h) if os.path.isfile(os.path.join(dir_np_chargrid_1h, f))]
+list_filenames = list_filenames[:10]
+
+
+def augment_data(data, tab_rand, order, shape, coord=False):
+    data_temp = resize(np.pad(data, ((tab_rand[1], tab_rand[3]), (tab_rand[0], tab_rand[2]), (0, 0)), 'constant'),
+                       shape, order=order, anti_aliasing=True)
+    if coord:
+        for i in range(0, nb_anchors):
+            mask = (data_temp > 1e-6)[:, :, 4 * i]
+            data_temp[mask, 4 * i] *= shape[1]
+            data_temp[mask, 4 * i] += tab_rand[0]
+            data_temp[mask, 4 * i] /= (tab_rand[0] + shape[1] + tab_rand[2])
+
+            data_temp[mask, 4 * i + 2] *= shape[1]
+            data_temp[mask, 4 * i + 2] += tab_rand[0]
+            data_temp[mask, 4 * i + 2] /= (tab_rand[0] + shape[1] + tab_rand[2])
+
+            data_temp[mask, 4 * i + 1] *= shape[0]
+            data_temp[mask, 4 * i + 1] += tab_rand[1]
+            data_temp[mask, 4 * i + 1] /= (tab_rand[1] + shape[0] + tab_rand[3])
+
+            data_temp[mask, 4 * i + 3] *= shape[0]
+            data_temp[mask, 4 * i + 3] += tab_rand[1]
+            data_temp[mask, 4 * i + 3] /= (tab_rand[1] + shape[0] + tab_rand[3])
+
+    return data_temp
+
+
+def extract_combined_data(dataset, batch_size, pad_left_range, pad_top_range, pad_right_range, pad_bot_range):
+    if batch_size > len(dataset): raise ValueError('batch_size > length of dataset {}'.format(len(dataset)))
+
+    np.random.shuffle(dataset)
+    tab_rand = np.random.rand(batch_size, 4) * [pad_left_range * width, pad_top_range * height, pad_right_range * width,
+                                                pad_bot_range * height]
+
+    tab_rand = tab_rand.astype(int)
+    chargrid_input, seg_gt, anchor_mask_gt, anchor_coord_gt = [], [], [], []
+
+    for i in range(0, batch_size):
+        data = np.load(os.path.join(dir_np_chargrid_1h, dataset[i]))
+        chargrid_input.append(augment_data(data, tab_rand[i], order=1, shape=(height, width, input_channels)))
+
+        data = np.load(os.path.join(dir_np_gt_1h, dataset[i]))
+        seg_gt.append(augment_data(data, tab_rand[i], order=1, shape=(height, width, nb_classes)))
+
+        data = np.load(os.path.join(dir_np_bbox_anchor_mask, dataset[i]))
+        anchor_mask_gt.append(augment_data(data, tab_rand[i], order=1, shape=(height, width, 2 * nb_anchors)))
+
+        data = np.load(os.path.join(dir_np_bbox_anchor_coord, dataset[i]))
+        anchor_coord_gt.append(
+            augment_data(data, tab_rand[i], order=0, shape=(height, width, 4 * nb_anchors), coord=True))
+
+    return np.array(chargrid_input), np.array(seg_gt), np.array(anchor_mask_gt), np.array(anchor_coord_gt)
+
+time_then = datetime.now()
+print(time_then)
+
+# Extract combined data here
+chargrid_input, seg_gt, anchor_mask_gt, anchor_coord = extract_combined_data(list_filenames, batch_size, pad_left_range,
+                                                                             pad_top_range, pad_right_range,
+                                                                             pad_bot_range)
+print((datetime.now() - time_then).total_seconds())
+class ChargridDataset(Dataset):
+    def __init__(self, chargrid_input, segmentation_ground_truth, anchor_mask_ground_truth, anchor_coordinates):
+        self.chargrid_input = chargrid_input
+        self.segmentation_ground_truth = segmentation_ground_truth
+        self.anchor_mask_ground_truth = anchor_mask_ground_truth
+        self.anchor_coordinates = anchor_coordinates
+        return
+
+    def __len__(self):
+        return len(self.chargrid_input)
+
+    def __getitem__(self, idx):
+        if type(idx) is torch.Tensor:
+            index = idx.item()
+
+        segmentation_label = self.segmentation_ground_truth[idx]
+        anchor_mask_label = self.anchor_mask_ground_truth[idx]
+        anchor_coordinates_label = self.anchor_coordinates[idx]
+        image = Image.fromarray(np.uint8(np.squeeze(self.chargrid_input[idx, :, :, 0:3] * 255)))
+        transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+        image = transforms(image)
+        return image, segmentation_label, anchor_mask_label, anchor_coordinates_label
+
+
+def get_dataset():
+    dataset = ChargridDataset(chargrid_input, seg_gt, anchor_mask_gt, anchor_coord)
+    # print('Dataset length is {0}'.format(len(dataset)))
+    test_no = int(len(dataset) * 0.2)
+    trainset, testset = random_split(dataset, [len(dataset) - test_no, test_no])
+
+    # print(len(trainset), len(testset))
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=5, shuffle=True, num_workers=0)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset), shuffle=True, num_workers=0)
+
+    return trainloader, testloader
+
+
+if __name__ == '__main__':
+    print('calling get_dataset')
+    trainloader, testloader = get_dataset()
+    img, l1, l2, l3 = next(iter(trainloader))
+
+    print(img.shape, l1.shape, l2.shape, l3.shape)
---
+++ ChargridNetwork.py
@@ -0,0 +1,250 @@
+from __future__ import print_function, division
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+def add_block_a(inputs, outputs, s=1):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, stride=s, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Dropout(p=0.1)
+    )
+
+
+def add_block_a_dash(inputs, outputs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, stride=2, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1, dilation=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1, dilation=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Dropout(p=0.1)
+    )
+
+
+def add_block_a_double_dash(inputs, outputs, d):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, kernel_size=3, padding=1, dilation=d),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1, dilation=d),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1, dilation=d),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Dropout(p=0.1)
+    )
+
+
+def add_block_b(inputs, outputs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, stride=2, kernel_size=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.ConvTranspose2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Dropout(p=0.1)
+    )
+
+
+def add_block_c(inputs, outputs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, stride=2, kernel_size=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.ConvTranspose2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+    )
+
+
+def add_block_d_or_e(inputs, outputs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Softmax(),
+    )
+
+
+def add_block_f(inputs, outputs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=inputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Conv2d(in_channels=outputs, out_channels=outputs, kernel_size=3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.BatchNorm2d(outputs),
+
+        nn.Linear(in_features=inputs, out_features=outputs),
+    )
+
+
+def concatenate_tensors(tensor1, tensor2):
+    (4, 64, 256, 128)
+    diff_height = tensor2.size()[2] - tensor1.size()[2]
+    diff_width = tensor2.size()[3] - tensor1.size()[3]
+
+    tensor1 = F.pad(tensor1, [diff_width // 2, diff_width - diff_width // 2, diff_height // 2, diff_height - diff_height // 2])
+    return torch.cat([tensor1, tensor2], dim=1)
+
+def shape_as_string(shape):
+    return str(list(shape))
+
+
+
+class ChargridNetwork(nn.Module):
+    def __init__(self, HW, C, num_classes=5, num_anchors=4):
+        super(ChargridNetwork, self).__init__()
+
+        '''
+        Encoder definition
+        '''
+        self.first_a_block = add_block_a(HW, C, 1)
+        self.second_a_block = add_block_a(C, 2*C, 2)
+        self.a_dash_block = add_block_a_dash(2*C, 4*C)
+        self.first_a_double_dash_block = add_block_a_double_dash(4*C, 8*C, d=2)
+        self.second_a_double_dash_block = add_block_a_double_dash(8*C, 8*C, d=4)
+
+        '''
+        Semantic Segmentation Decoder (ssd)
+        '''
+        self.ssd_first_b_block = add_block_b(12*C, 4*C)
+        self.ssd_second_b_block = add_block_b(6*C, 2*C)
+        self.ssd_c_block = add_block_c(3*C, C)
+        self.ssd_d_block = add_block_d_or_e(C, C)
+
+        '''
+        Bounding Box Regression Decoder (bbrd)
+        '''
+
+        self.bbrd_first_b_block = add_block_b(12*C, 4*C)
+        self.bbrd_second_b_block = add_block_b(6*C, 2*C)
+        self.bbrd_c_block = add_block_c(3*C, C)
+        self.bbrd_e_block = add_block_d_or_e(C, C)
+        self.bbrd_f_block = add_block_f(C, C)
+
+    def forward(self, x):
+        """
+        Forward pass function for the network
+        :param x:
+        :return:
+        """
+
+        '''
+        Forward pass through Encoder
+        '''
+        print("input shape " + shape_as_string(x.shape))
+
+        first_a_block_output = self.first_a_block(x)
+        print("after first a block " + shape_as_string(first_a_block_output.shape))
+
+        second_a_block_output = self.second_a_block(first_a_block_output)
+        print("after second a block " + shape_as_string(second_a_block_output.shape))
+
+        a_dash_block_output = self.a_dash_block(second_a_block_output)
+        print("after a dash block " + shape_as_string(a_dash_block_output.shape))
+
+        first_a_double_dash_block_output = self.first_a_double_dash_block(a_dash_block_output)
+        print("after first a double dash block " + shape_as_string(first_a_double_dash_block_output.shape))
+
+        second_a_double_dash_block_output = self.second_a_double_dash_block(first_a_double_dash_block_output)
+        print("after second a double dash block " + shape_as_string(second_a_double_dash_block_output.shape))
+
+        '''
+        Forward pass through semantic segmentation decoder
+        '''
+        ssd_first_b_block_input = concatenate_tensors(second_a_double_dash_block_output, a_dash_block_output)
+        ssd_first_b_block_output = self.ssd_first_b_block(ssd_first_b_block_input)
+        print("ssd after first b block " + shape_as_string(ssd_first_b_block_output.shape))
+
+        ssd_second_b_block_input = concatenate_tensors(ssd_first_b_block_output, second_a_block_output)
+        ssd_second_b_block_output = self.ssd_second_b_block(ssd_second_b_block_input)
+        print("ssd after second b block " + shape_as_string(ssd_second_b_block_output.shape))
+
+        ssd_c_block_input = concatenate_tensors(ssd_second_b_block_output, first_a_block_output)
+        ssd_c_block_output = self.ssd_c_block(ssd_c_block_input)
+        print("ssd after c block " + shape_as_string(ssd_c_block_output.shape))
+
+        d_block_output = self.ssd_d_block(ssd_c_block_output)
+        print("ssd after d block " + shape_as_string(d_block_output.shape))
+
+        '''
+        Forward pass through bounding box regression decoder
+        '''
+        bbrd_first_b_block_input = concatenate_tensors(second_a_double_dash_block_output, a_dash_block_output)
+        bbrd_first_b_block_output = self.bbrd_first_b_block(bbrd_first_b_block_input)
+        print("bbrd after first b block " + shape_as_string(bbrd_first_b_block_output.shape))
+
+        bbrd_second_b_block_input = concatenate_tensors(bbrd_first_b_block_output, second_a_block_output)
+        bbrd_second_b_block_output = self.bbrd_second_b_block(bbrd_second_b_block_input)
+        print("bbrd after second b block " + shape_as_string(bbrd_second_b_block_output.shape))
+
+        bbrd_c_block_input = concatenate_tensors(bbrd_second_b_block_output, first_a_block_output)
+        bbrd_c_block_output = self.bbrd_c_block(bbrd_c_block_input)
+        print("bbrd after c block " + shape_as_string(bbrd_c_block_output.shape))
+
+        bbrd_e_block_output = self.bbrd_e_block(bbrd_c_block_output)
+        print("bbrd after e block " + shape_as_string(bbrd_e_block_output.shape))
+
+        bbrd_f_block_output = self.bbrd_e_block(bbrd_c_block_output)
+        print("bbrd after e block " + shape_as_string(bbrd_f_block_output.shape))
+        
+
+
+
+import ChargridDataset
+
+trainloader, testloader = ChargridDataset.get_dataset()
+
+net = ChargridNetwork(3, 64, 5, 4)
+img, l1, l2, l3 = next(iter(trainloader))
+net(img)
